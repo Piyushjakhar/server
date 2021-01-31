4a76515e4e720db39fe1c8f8d446ad99ce148ea9
Piyush Jakhar <piyushjakhar08@gmail.com>
2021-02-01 01:24:01 +0530

    authflow completed

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+//prod.js
+
+module.exports = {
+    googleCLientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookiekey: process.env.COOKIE_KEY 
+};
